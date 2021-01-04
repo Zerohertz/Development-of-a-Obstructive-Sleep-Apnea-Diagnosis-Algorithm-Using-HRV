ca1aff983815a2894132f44eddfdd8a1754cd6b7
오효근 <zerohertz@zerohertz.local>
2021-01-04 22:28:24 +0900

    freqTotal

--- windowHRV.m
+++ windowHRV.m
@@ -1,10 +1,8 @@
 function [f, P] = windowHRV(time, ECG, SamplingRate, Winsize)
 %Sampling Rate(Hz)
 %Window size(Min)
-%Time_Arr = [];
-%ECG_Arr = [];
-Win = fix(SamplingRate * Winsize * 60);
 SamplingTime = Winsize * 60;
+Win = fix(SamplingRate * SamplingTime);
 num = fix(length(ECG) / Win);
 
 for N = (1:num)
@@ -12,9 +10,14 @@ for N = (1:num)
     ECG_Arr(:, N) = ECG(Win * (N - 1) + 1:Win * N);
 end
 
+f = NaN(1000, num);
+P = NaN(1000, num);
+
 for N = (1:num)
-    [qrs(:, N), locs(:, N), y(:, N)] = rr_interval(Time_Arr(:, N), ECG_Arr(:, N));
-    [ti(:, N), HRV(:, N)] = MakeHRV(locs(:, N));
-    [f(:, N), P(:, N)] = freqHRV(HRV(:, N), length(HRV), SamplingTime);
+    [qr, lo, y(N, :)] = rr_interval(Time_Arr(:, N), ECG_Arr(:, N));
+    [ti, HRV] = MakeHRV(lo);
+    [fr, P1] = freqHRV1(HRV, length(HRV), SamplingTime);
+    f(1:length(fr), N) = fr;
+    P(1:length(P1), N) = P1;
 end
 end
