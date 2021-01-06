56d767cd87f450641a03c2ccac4e4389d4ec8d31
오효근 <zerohertz@zerohertz.local>
2021-01-06 15:25:08 +0900

    format

--- freqHRV.m
+++
@@ -1,9 +0,0 @@
-function [f, P1] = freqHRV(hrv, Length, SamplingTime)
-y=fft(hrv);
-SamplingRate = Length / SamplingTime;
-P2 = abs(y/Length);
-P1 = P2(1:fix(Length/2)+1);
-P1(2:end-1) = 2*P1(2:end-1);
-f = SamplingRate * (0:(Length/2))/Length;
-end
-
--- makeHRV.m
+++
@@ -1,7 +0,0 @@
-function [time, val] = makeHRV(locat)
-for num = (1:length(locat)-1)
-    val(num) = locat(num + 1) - locat(num);
-    time(num) = locat(num);
-end
-end
-
